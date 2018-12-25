0fc01bde0111542be8b491167c6bf796044e13a0
sousarka <soumik.sarkar@gmail.com>
2018-12-25 08:10:32 -0700

    add test project

---
+++ test/intg-test/src/test/com/ss/IntgTest.java
@@ -0,0 +1,20 @@
+package com.ss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntgTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
